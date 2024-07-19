d32882e00dde40eb5cb75f941ad1c681502f6c1d
Rifat <rifat.khaidir@gmail.com>
2024-07-19 16:20:21 +0800

    add new div

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 function calculateBMI() {
   const heighInput = document.getElementById("height");
   const weightInput = document.getElementById("weight");
-  const resultDiv = document.getElementById("result");
+  const outputDiv = document.getElementById("output");
 
   const height = parseFloat(heighInput.value);
   const weight = parseFloat(weightInput.value);
 
   if (isNaN(height) || isNaN(weight)) {
-    resultDiv.innerHTML = "Please enter valid height and weight";
+    outputDiv.innerHTML = `<div class="container">Please enter valid value</div>`;
     return;
   }
 
   const bmi = weight / (height / 100) ** 2;
   let category = "";
+  let explanation = "";
 
   if (bmi < 18.5) {
     category = "Underweight";
+    explanation = "Kurang makan";
   } else if (bmi < 25) {
     category = "Normal Weight";
+    explanation = "lumayan normal lahh";
   } else if (bmi < 30) {
     category = "Overweight";
+    explanation = "jarang olahraga ya, ngemil terus sihh";
   } else {
     category = "Obese";
+    explanation = "waduhh parah banget !!!";
   }
 
-  resultDiv.innerHTML = `Your BMI is ${bmi.toFixed(2)} (${category})`;
+  // resultDiv.innerHTML = `Your BMI is ${bmi.toFixed(2)} (${category})`;
+
+  outputDiv.innerHTML = `<div class="container">
+      <div id="output">Your BMI is ${bmi.toFixed(2)} (${category})</div>
+      <div id="explanation">Based on your BMI the explanation is : <br> </div>
+      <p> ${explanation}</p>
+    </div> `;
 }
 
 function clearBMI() {
   document.getElementById("height").value = "";
   document.getElementById("weight").value = "";
-  document.getElementById("result").innerHTML = "";
+  document.getElementById("output").innerHTML = "";
 }
